b72ff94e0a9edbd511437736d39c848ad2d8fff6
Kim Bjerge <au288681@uni.au.dk>
2024-08-26 14:16:12 +0200

    Update training-ClassificationExtended19Cls.py

--- python/training-ClassificationExtended19Cls.py
+++ python/training-ClassificationExtended19Cls.py
@@ -15,7 +15,7 @@ import argparse
 
 ## LIMIT MEMORY - Can be uncommented
 config = tf.compat.v1.ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.4
+config.gpu_options.per_process_gpu_memory_fraction = 0.95
 session = tf.compat.v1.Session(config=config)
 
 #from tensorboard.plugins.hparams import api as hp
